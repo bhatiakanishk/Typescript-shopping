62cd9ee3437a85c5b6b603a58301d0cb6bee2069
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-07-25 19:51:46 -0400

    Create StoreItem.tsx

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,14 @@
+import { Card } from "react-bootstrap"
+
+type StoreItemProps = {
+    id: number
+    name: string
+    price: number
+    imgURL: string
+}
+
+export function StoreItem({ id, name, price, imgURL }: StoreItemProps) {
+    return <Card>
+        <Card.Img variant="top" src={imgURL} height="200px" style={{objectFit: "cover"}}/>
+    </Card>
+}
